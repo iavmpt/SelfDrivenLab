05541603b881490f0c36430bbeca47a699203123
isaac madrigal <isaac.madrigal@student.centralia.edu>
2024-03-11 12:37:11 -0700

    Added customer first name, last name, and number

--- angularapp/Models/Customer/CustomerInfo.cs
+++ angularapp/Models/Customer/CustomerInfo.cs
@@ -4,8 +4,40 @@ class Program
 {
   static void Main()
   {
-    cusotmer obj = new cusotmer();
+    using System;
 
+class Customer
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+
+    public Customer(string firstName, string lastName, string phoneNumber)
+    {
+      FirstName = firstName;
+      LastName = lastName;
+      PhoneNumber = phoneNumber;
+    }
+
+    public void DisplayInfo()
+    {
+      Console.WriteLine($"Customer: {FirstName} {LastName}");
+      Console.WriteLine($"Phone Number: {PhoneNumber}");
+    }
   }
+
+  class Program
+  {
+    static void Main()
+    {
+      // Creating an instance of the Customer class
+      Customer customer1 = new Customer("Isaac", "Madrigal", "123-456-7890");
+
+      // Displaying customer information
+      customer1.DisplayInfo();
+    }
+  }
+
+}
 }
 
